321f98266cc20d0aee621ba53f95a4caa38d22e9
Ezequiel Zalazar <ezebj39@gmail.com>
2023-08-29 23:57:41 -0300

    Practico 2 terminado

---
+++ Practico2/readme.md
@@ -0,0 +1,28 @@
+# Juego de Piedra, Papel o Tijera - Al Mejor de 5
+
+## Descripción
+Este es un juego simple de "Piedra, Papel o Tijera" donde el objetivo es ser el primero en llegar a 3 victorias. En cada ronda, el jugador elige una de las tres opciones, y luego se compara con la elección del oponente (PC). El juego continuará hasta que uno de los jugadores alcance las 3 victorias.
+
+## Reglas del Juego
+- Piedra vence a Tijera.
+- Tijera vence a Papel.
+- Papel vence a Piedra.
+
+## Cómo Jugar
+1. Ingresa tu nombre en el campo correspondiente y haz clic en el botón "Jugar".
+2. En cada ronda, selecciona tu opción haciendo clic en la imagen de "piedra", "papel" o "tijera".
+3. El oponente (PC) también elegirá una opción aleatoriamente.
+4. El resultado de la ronda se mostrará en pantalla.
+5. El juego continuará hasta que uno de los jugadores alcance 3 victorias.
+
+## Ejemplo de Ganar
+- Si ganas una ronda, obtendrás un punto.
+- El primer jugador en llegar a 3 puntos gana el juego.
+
+## Notas Adicionales
+- ¡Diviértete y disfruta del juego!
+
+## Créditos
+Este juego fue creado con HTML, CSS y JavaScript por Ezequiel.
+
+**¡Disfruta el juego!** 🎮✌️
---
+++ Practico2/script.js
@@ -0,0 +1,147 @@
+//Eleccion del usuario empleada en funcion de juego
+let eleccionUsuario;
+//Nombre del usuario empleado para personalizar mensajes
+let usuario;
+//Variable donde se lleva el conteo de puntos del Usuario
+let puntosUsuario = 0;
+//Variable donde se lleva el conteo de puntos de la Computadora
+let puntosComputadora = 0; 
+
+//Paso a variable todos los elementos con el metodo querySelectorpara que al necesitar usarlos, sea mas facil llamarlos.
+const nombreUsuario = document.querySelector("#nombreUsuario");
+
+const inicio = document.querySelector(".inicio");
+
+const mensajeBienvenida = document.querySelector(".mensajeBienvenida");
+
+const personalizado = document.querySelector("#personalizado");
+
+
+const botonera = document.querySelector(".botonera");
+
+const piedra = document.getElementById("piedra");
+piedra.addEventListener("click", () => enJuego("piedra"));
+
+
+const papel = document.getElementById("papel");
+papel.addEventListener("click", () => enJuego("papel"));
+
+const tijera = document.getElementById("tijera");
+tijera.addEventListener("click", () => enJuego("tijera"));
+
+const info = document.querySelector(".info");
+
+const jugadaUsuario = document.querySelector("#jugadaUsuario");
+
+const jugadaComputadora = document.querySelector("#jugadaComputadora");
+
+const ganador = document.querySelector("#ganador");
+
+const contadorUsuario = document.querySelector("#contadorUsuario");
+
+
+const contadorComputadora = document.querySelector("#contadorComputadora");
+
+const resultadoFinal = document.querySelector(".resultadoFinal");
+
+const ganadorFinal = document.querySelector("#ganadorFinal");
+
+//Funcion para conseguir nombre del jugador
+function traerNombre(id) {
+
+    if ((document.querySelector(id).value) === ``) {
+        alert("ERROR: Tenes que ingresar tu nombre");
+        return;
+    }
+    else {
+        let nombre = document.querySelector(id).value
+        return nombre;
+    }
+
+}
+
+//Funcion, con un randomizador, para obtener la jugada de la Computadora
+function obtenerJugadaComputadora() {
+    let opciones = ["piedra", "papel", "tijera"];
+    let jugada = opciones[(Math.floor(Math.random() * 3))];
+    return jugada;
+}
+
+//Funcion que determina, entre dos jugadas el ganador
+function determinarGanador(jugada1, jugada2) {
+    if (jugada1 == jugada2) {
+        return "Empate";
+    }
+    else if ((jugada1 == "piedra" && jugada2 == "tijera") ||
+        (jugada1 == "papel" && jugada2 == "piedra") ||
+        (jugada1 == "tijera" && jugada2 == "papel")) {
+        return "Usuario";
+    }
+    else {
+        return "Computadora";
+    }
+}
+
+//Funcion donde se especifican las acciones de cada turno jugado
+function jugarTurno(eleccionUsuario) {
+
+    mensajeBienvenida.style.display = "none";
+
+    jugadaUsuario.innerText = `${usuario} eligio: ${eleccionUsuario}`;
+
+    let eleccionComputadora = obtenerJugadaComputadora();
+    jugadaComputadora.innerText = `La Computadora eligio: ${eleccionComputadora}`;
+
+    let resultado = determinarGanador(eleccionUsuario, eleccionComputadora);
+
+    if (resultado === "Empate") {
+        ganador.innerText = "Empate...";
+    }
+    else if (resultado === "Usuario") {
+        ganador.innerText = `Un punto para ${usuario}!!!`;
+        puntosUsuario += 1;       
+    }
+    else {
+        ganador.innerText = "Un punto para la Computadora!!!";
+        puntosComputadora += 1;        
+    }
+
+    contadorUsuario.innerText = `Puntos de ${usuario}: ${puntosUsuario}`;
+    contadorComputadora.innerText = `Puntos de la Computadora: ${puntosComputadora}`;
+
+}
+
+//Funcion que pone a correr el juego llamando hasta que alguien gane (accionada por los botones de P/P/T)
+function enJuego(jugadaElegida) {
+
+    if (puntosUsuario < 5 && puntosComputadora < 5) {
+        jugarTurno(jugadaElegida);
+    }
+    if (puntosUsuario === 3 ||
+        puntosComputadora === 3 ||
+        (Math.abs(puntosUsuario - puntosComputadora)) === 3) {
+
+        botonera.style.display = "none";
+        info.style.display = "none";
+
+        resultadoFinal.style.display = "flex";
+
+        if (puntosUsuario > puntosComputadora) {
+            ganadorFinal.innerText = "GANASTE";
+        }
+        else {
+            ganadorFinal.innerText = "PERDISTE";
+        }
+    }
+}
+
+//Al accionar el boton inicial, se determina el nombre de usuario, el boton se esconde y se muestra la botonera.
+go.addEventListener("click", () => {
+    usuario = traerNombre("#nombreUsuario");  
+    if(usuario !== undefined){
+        personalizado.innerText = `Bienvenid@ ${usuario}!!!`;
+        mensajeBienvenida.style.display = "flex";
+        botonera.style.display = "block";
+        inicio.style.display = "none"; 
+    }    
+});
